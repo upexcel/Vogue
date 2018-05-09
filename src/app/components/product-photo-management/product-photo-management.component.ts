c74abfeae081d64e86963b5b1247b185accf30bd
upexcel <rahul.excel2011@gmail.com>
2018-05-09 20:56:02 +0530

    Added Table for relation

--- src/app/components/product-photo-management/product-photo-management.component.ts
+++ src/app/components/product-photo-management/product-photo-management.component.ts
@@ -3,19 +3,23 @@ import { NgForm } from '@angular/forms';
 import { FileUploader } from 'ng2-file-upload';
 import { environment } from '../../../environments/environment';
 import { IntermediateStorageService } from '../../services/intermediateStorage.service';
+import { LocalStorageService } from '../../services/local-storage.service';
 
 @Component({
   selector: 'app-product-photo-management',
   templateUrl: './product-photo-management.component.html',
   styleUrls: ['./product-photo-management.component.css']
 })
-export class ProductPhotoManagementComponent implements OnDestroy{
+export class ProductPhotoManagementComponent implements OnDestroy {
   @ViewChild('image')
   resetImage: any;
   typeError: boolean;
   imageUploader: FileUploader;
+  imageTable: any;
+  imageTableError: any;
+  response: any;
 
-  constructor(public intermediateStorageService: IntermediateStorageService) {
+  constructor(public intermediateStorageService: IntermediateStorageService, public localStorageService: LocalStorageService) {
     this.imageUploader = new FileUploader({
       url: `${environment['apiHost']}imageProcess/uploadImage`,
 
@@ -24,7 +28,7 @@ export class ProductPhotoManagementComponent implements OnDestroy{
       autoUpload: true,
     });
 
-    if(this.intermediateStorageService.storeImageData) {
+    if (this.intermediateStorageService.storeImageData) {
       this.imageUploader = this.intermediateStorageService.getImageData();
     }
 
@@ -32,14 +36,19 @@ export class ProductPhotoManagementComponent implements OnDestroy{
       item.withCredentials = false;
     }
 
-    this.imageUploader.response.subscribe(res => {
+    this.imageUploader.response.subscribe((res) => {
       if (res) {
-        console.log(res);
-      };
+        this.response = JSON.parse(res);
+        this.imageTable = this.response['data'].length ? this.response['data'] : null;
+        this.imageTableError = this.response['errors'].length ? this.response['errors'] : null;
+        console.log(this.response);
+      }
+      console.log(this.imageTable);
+      console.log(this.imageTableError);
       this.reset();
     });
   }
-
+  
   checkType(event) {
     if (this.imageUploader.queue.length > 1) this.imageUploader.queue.shift();
     if (event.target.files[0]) {
@@ -48,16 +57,18 @@ export class ProductPhotoManagementComponent implements OnDestroy{
         this.typeError = true;
         this.imageUploader.clearQueue();
       } else {
-        this.typeError = false;        
+        this.typeError = false;
       }
     }
   }
-
+  
   reset() {
     this.resetImage.nativeElement.value = ""
   }
-
+  
   ngOnDestroy() {
     this.intermediateStorageService.setImageData(this.imageUploader);
+    this.reset();
+    this.imageUploader.clearQueue();
   }
 }
