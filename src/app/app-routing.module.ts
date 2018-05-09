e4249c6146c9865c1ee237209606a51b80fbd7b3
upexcel <rahul.excel2011@gmail.com>
2018-05-09 12:30:37 +0530

    Used HASH Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -41,7 +41,12 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(
+      routes,
+      {useHash: true},
+    )
+  ],
   exports: [RouterModule]
 })
 
