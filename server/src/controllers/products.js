ee030ecd492f963fcb739d6082cf07363a8a5274
nodeexcel <reactexcel>
2018-06-08 14:14:35 +0530

    upload products by xml file

--- server/src/controllers/products.js
+++ server/src/controllers/products.js
@@ -6,60 +6,47 @@ import formidable from "formidable";
 import fs from "fs";
 import csvjson from "csvjson";
 import path from "path";
+var to_json = require('xmljson').to_json;
+var x2j = require('xml2js');
 
 export class UserController extends BaseAPIController {
 
     createProducts = (req, res, next) => {
+        var arrToDelete = []
         let form = new formidable.IncomingForm();
         form.parse(req, function(err, fields, files) {
             if (files.file) {
-                if (files.file['name'].substr(files.file['name'].lastIndexOf('.') + 1).toLowerCase() != 'csv') {
+                if (files.file['name'].substr(files.file['name'].lastIndexOf('.') + 1).toLowerCase() != 'xml') {
                     res.status(400).json({ error: 1, message: "please upload csv file" })
                 } else {
-                    fs.readFile(files.file.path, function(err, data) {
-                        var csv = data.toString('utf8')
-                        var lines = csv.split("\n");
-                        var result = [];
-                        var headers = lines[0].split("|");
-                        for (var i = 1; i < lines.length; i++) {
-                            var obj = {};
-                            var currentline = lines[i].split('|');
-                            for (var j = 0; j < headers.length; j++) {
-
-                                obj[headers[j]] = currentline[j] ? currentline[j] : null;
-                            }
-                            result.push(obj);
-                        }
-                        db.products.findAll({}).then((resp) => {
-                            var arrToDelete = []
-                            _.map(resp, (val, key) => {
-                                let findProduct = _.find(result, function(get) { return get.ProductID == val.ProductID; });
-                                if (!findProduct) {
-                                    arrToDelete.push(val.ProductID)
+                    db.products.findAll({}).then((resp) => {
+                        fs.readFile(files.file.path, function(err, data) {
+                            let xml = data.toString('utf8')
+                            to_json(xml, function(error, data) {
+                                if (error) {
+                                    res.status(400).json({ error: 1, message: "something is wrong in your xml file", data: error })
+                                } else {
+                                    let products = data.Products.Product
+                                    let arr = [];
+                                    arrayOfJson(products, arr, function(jsonArray) {
+                                        let arrToDelete = []
+                                        _.map(resp, (val, key) => {
+                                            let findProduct = _.find(jsonArray, function(get) { return get.ProductID == val.ProductID; });
+                                            if (!findProduct) {
+                                                arrToDelete.push(val.ProductID)
+                                            }
+                                            if (key == resp.length - 1) {
+                                                db.products.destroy({ where: { ProductID: arrToDelete } }).then((data) => {
+                                                    console.log(data, "deleted")
+                                                })
+                                            }
+                                        })
+                                        indertUpdateProducts(jsonArray, function(finalResponse) {
+                                            res.json({ status: 1, message: finalResponse })
+                                        })
+                                    })
                                 }
-                            })
-                            db.products.destroy({ where: { ProductID: arrToDelete } }).then((data) => {
-                                console.log(data, "deleted")
-                            })
-                        })
-
-                        _.forEach(result, (val, key) => {
-                            delete val.ImageFullsizeURL;
-                            if (val.ProductID) {
-                                db.products.findOne({ where: { ProductID: val.ProductID } }).then((product) => {
-                                    if (product) {
-                                        db.products.update(val, { where: { ProductID: val.ProductID } }).then((data) => {})
-                                    } else {
-                                        db.products.create(val).then((data) => {})
-                                    }
-                                })
-
-                            } else {
-                                console.log("productID not exist")
-                            }
-                            if (key == result.length - 1) {
-                                res.json({ status: 1, message: "success" })
-                            }
+                            });
                         })
                     })
                 }
@@ -67,6 +54,48 @@ export class UserController extends BaseAPIController {
                 res.status(400).json({ error: 1, message: "file not recieved" })
             }
         })
+
+        function arrayOfJson(products, jsonArray, callback) {
+            if (Object.keys(products).length) {
+                for (var x in products) {
+                    products[x].Colour1 = products[x].colors.Colour1;
+                    products[x].Colour2 = products[x].colors.Colour2;
+                    products[x].Colour3 = products[x].colors.Colour3;
+                    products[x].Subcategory = products[x].subcategories.Subcategory['1'];
+                    jsonArray.push(products[x]);
+                    if (x == Object.keys(products).length - 1) {
+
+                        callback(jsonArray)
+                    }
+                }
+            } else {
+                callback(jsonArray)
+            }
+        }
+
+        function indertUpdateProducts(data, callback) {
+            if (data.length) {
+                let val = data.splice(0, 1)[0];
+                if (val.ProductID) {
+                    db.products.findOne({ where: { ProductID: val.ProductID } }).then((product) => {
+                        if (product) {
+                            db.products.update(val, { where: { ProductID: val.ProductID } }).then((data) => {})
+                        } else {
+                            db.products.create(val).then((data) => {})
+                        }
+                    })
+                } else {
+                    console.log("productID not exist")
+                }
+                if (data.length) {
+                    indertUpdateProducts(data, callback)
+                } else {
+                    callback("success")
+                }
+            } else {
+                callback(data)
+            }
+        }
     }
 
     getProducts = (req, res, next) => {
